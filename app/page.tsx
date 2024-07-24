d4575e2567bc18bd9173008eeb5ac8e4224bd9c1
Jonathan Alvarez <jonathan@litentry.com>
2024-07-24 10:11:27 -0500

    update litentry pkgs
    Signed-off-by: Jonathan Alvarez <jonathan@litentry.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { ApiPromise, WsProvider } from "@polkadot/api";
+
 export default function Home() {
-    return (
-        <main>
-            <h1>Home</h1>
-        </main>
-    )
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
 }
